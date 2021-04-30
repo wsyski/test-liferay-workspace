771c127f8fe111b2e921f5ceb7622b6e6d476b24
wsyski <wsyski@gmail.com>
2021-04-30 20:00:43 +0200

    Initial version

---
+++ modules/react-cross-circle/src/main/java/react/cross/circle/constants/ReactCrossCirclePortletKeys.java
@@ -0,0 +1,10 @@
+package react.cross.circle.constants;
+
+/**
+ * @author wos
+ */
+public class ReactCrossCirclePortletKeys {
+
+	public static final String ReactCrossCircle = "react_cross_circle";
+
+}
